c60e726820c001314e549513982ce9c689780ae1
GianmarcoBramucci <giangian797@gmail.com>
2024-05-28 21:46:43 +0200

    make table whit migration

--- database/migrations/2024_05_28_191819_create_trains_table.php
+++ database/migrations/2024_05_28_191819_create_trains_table.php
@@ -12,7 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('trains', function (Blueprint $table) {
-            $table->id();
+            $table->id('codice_treno');
+            $table->string('azienda', 100);
+            $table->string('stazione_di_partenza', 100);
+            $table->string('stazione_di_arrivo', 100);
+            $table->time('orario_di_partenza', $precision = 0);
+            $table->time('orario_di_arrivo', $precision = 0);
+            $table->integer('numero_carrozze');
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
             $table->timestamps();
         });
     }
